e799930ff63cc661166c29d53aca092ae4f273d7
TaeGyeomHwang <jihuhw99@gmail.com>
2024-01-03 16:27:35 +0900

    App.js 주석 추가

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ function App() {
     <BrowserRouter>
       <MainTileText>태겸의 미니 블로그</MainTileText>
       <Routes>
+        {/* slug를 설정해준다. */}
+        {/* 그냥 / 하나는 메인페이지와 같다. 메인 페이지 형태도 localhost:3000/ 이므로. */}
         <Route index element = {<MainPage />} />
         <Route path="post-write" element = {<PostWritePage />} />
         <Route path="post/:postId" element = {<PostViewPage />} />
